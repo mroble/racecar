ba08ed06d6acd8220f46aad104c846ab4f6a0e52
Maureen Roble <moroble@gmail.com>
2016-12-11 09:29:39 -0500

    Add beginLoadImage helper function

--- ImageLoading.js
+++ ImageLoading.js
@@ -12,19 +12,13 @@ function countLoadedImagesAndLaunchIfReady() {
 	}
 }
 
-function carImageLoad() {
-	carPic.onload = countLoadedImagesAndLaunchIfReady;
-	carPic.src = "player1car.png";
-}
-
-function trackLoadImages() {
-	roadPic.onload = countLoadedImagesAndLaunchIfReady;
-	wallPic.onload = countLoadedImagesAndLaunchIfReady;
-	roadPic.src = "track_road.png";
-	wallPic.src = "track_wall.png";
+function beginLoadingImage(imgVar, fileName) {
+	imgVar.onload = countLoadedImagesAndLaunchIfReady;
+	imgVar.src = fileName;
 }
 
 function loadImages() {
-	carImageLoad();
-	trackLoadImages();
+	beginLoadingImage(carPic, "player1car.png");
+	beginLoadingImage(roadPic, "track_road.png");
+	beginLoadingImage(wallPic, "track_wall.png");
 }
