b713b58014ca117449798335f04f3b18fc777a3c
Maureen Roble <moroble@gmail.com>
2016-12-12 12:56:43 -0500

    add trees and goal post

--- ImageLoading.js
+++ ImageLoading.js
@@ -1,12 +1,15 @@
 var carPic = document.createElement("img");
 var roadPic = document.createElement("img");
 var wallPic = document.createElement("img");
+var goalPic = document.createElement("img");
+var treePic = document.createElement("img");
+var flagPic = document.createElement("img");
 
 var picsToLoad = 0; // set automatically based on imageList in loadImages()
 
 function countLoadedImagesAndLaunchIfReady() {
 	picsToLoad--;
-	console.log(picsToLoad);
+	//console.log(picsToLoad);
 	if(picsToLoad == 0) {
 		imageLoadingDoneSoStartGame();
 	}
@@ -18,13 +21,14 @@ function beginLoadingImage(imgVar, fileName) {
 }
 
 function loadImages() {
-	// next line is just an example, not using, will remove later
-	var dataSet = {varName: carPic, theFile: "player1car.png"};
-
+	
 	var imageList = [
 		{varName: carPic, theFile: "player1car.png"},
 		{varName: roadPic, theFile: "track_road.png"},
-		{varName: wallPic, theFile: "track_wall.png"}
+		{varName: wallPic, theFile: "track_wall.png"},
+		{varName: goalPic, theFile: "track_goal.png"},
+		{varName: treePic, theFile: "track_tree.png"},
+		{varName: flagPic, theFile: "track_flag.png"}
 		];
 
 	picsToLoad = imageList.length;
--- Track.js
+++ Track.js
@@ -3,9 +3,9 @@ const TRACK_H = 40;
 const TRACK_GAP = 2;
 const TRACK_COLS = 20;
 const TRACK_ROWS = 15;
-var trackGrid = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-				 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-				 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+var trackGrid = [4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+				 4, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+				 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
 				 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1,
 				 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1,
 				 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1,
@@ -22,8 +22,11 @@ var trackGrid = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 const TRACK_ROAD = 0;
 const TRACK_WALL = 1;
 const TRACK_PLAYERSTART = 2;
+const TRACK_GOAL = 3;
+const TRACK_TREE = 4;
+const TRACK_FLAG = 5;
 
-function isWallAtColRow(col, row) {
+function isObstacleAtColRow(col, row) {
 	if(col >= 0 && col < TRACK_COLS &&
 		row >= 0 && row < TRACK_ROWS) {
 		 var trackIndexUnderCoord = rowColToArrayIndex(col, row);
@@ -41,7 +44,7 @@ function carTrackHandling() {
 	if(carTrackCol >= 0 && carTrackCol < TRACK_COLS &&
 		carTrackRow >= 0 && carTrackRow < TRACK_ROWS) {
 
-		if(isWallAtColRow( carTrackCol,carTrackRow )) {
+		if(isObstacleAtColRow( carTrackCol,carTrackRow )) {
 			// next two lines added to fix a bug, mentioned in video 9.6
 			// undoes the car movement which got it onto the wall
 			carX -= Math.cos(carAng) * carSpeed;
@@ -62,14 +65,29 @@ function drawTracks() {
 		for(var eachCol=0;eachCol<TRACK_COLS;eachCol++) {
 
 			var arrayIndex = rowColToArrayIndex(eachCol, eachRow); 
+			var tileKindHere = trackGrid[arrayIndex];
+			var useImg;
 
-			if(trackGrid[arrayIndex] == TRACK_ROAD) {
-				canvasContext.drawImage(roadPic,
-					TRACK_W*eachCol,TRACK_H*eachRow);
-			} else if(trackGrid[arrayIndex] == TRACK_WALL) {
-				canvasContext.drawImage(wallPic,
-					TRACK_W*eachCol,TRACK_H*eachRow);
-			}
+			switch(tileKindHere) {
+				case TRACK_ROAD: 
+					useImg = roadPic;
+					break;
+				case TRACK_WALL: 
+					useImg = wallPic;
+					break;
+				case TRACK_GOAL: 
+					useImg = goalPic;
+					break;
+				case TRACK_TREE: 
+					useImg = treePic;
+					break;
+				case TRACK_FLAG: 
+					useImg = flagPic;
+					break;
+			
+
+			canvasContext.drawImage(roadPic, TRACK_W*eachCol,TRACK_H*eachRow);
+
 		} // end of for each col
 	} // end of for each row
 
